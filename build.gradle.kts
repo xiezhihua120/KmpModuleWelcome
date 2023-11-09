ab4b9e7047b0c5d2a4112d0b621c840634ec9f88
xiezhihua <xiezhihua@beemwork.com>
2023-11-09 19:59:43 +0800

    feat: add welcome module

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,5 +1,6 @@
 plugins {
-    kotlin("multiplatform") version "1.9.0"
+    kotlin("multiplatform")
+    id("com.android.library")
 }
 
 group = "me.allin327"
@@ -9,50 +10,73 @@ repositories {
     mavenCentral()
 }
 
+@OptIn(org.jetbrains.kotlin.gradle.ExperimentalKotlinGradlePluginApi::class)
 kotlin {
-    jvm {
-        jvmToolchain(8)
-        withJava()
-        testRuns.named("test") {
-            executionTask.configure {
-                useJUnitPlatform()
+    targetHierarchy.default()
+
+    // android平台
+    androidTarget {
+        compilations.all {
+            kotlinOptions {
+                jvmTarget = "1.8"
             }
         }
     }
-    js {
-        browser {
-            commonWebpackConfig {
-                cssSupport {
-                    enabled.set(true)
-                }
-            }
-        }
-    }
+
+    // ios平台
+    listOf(
+        iosX64(),
+        iosArm64(),
+        iosSimulatorArm64()
+    )
+
+    // native平台
     val hostOs = System.getProperty("os.name")
     val isArm64 = System.getProperty("os.arch") == "aarch64"
     val isMingwX64 = hostOs.startsWith("Windows")
     val nativeTarget = when {
-        hostOs == "Mac OS X" && isArm64 -> macosArm64("native")
-        hostOs == "Mac OS X" && !isArm64 -> macosX64("native")
-        hostOs == "Linux" && isArm64 -> linuxArm64("native")
-        hostOs == "Linux" && !isArm64 -> linuxX64("native")
-        isMingwX64 -> mingwX64("native")
+        hostOs == "Mac OS X" && isArm64 -> macosArm64("nativePc")
+        hostOs == "Mac OS X" && !isArm64 -> macosX64("nativePc")
+        isMingwX64 -> mingwX64("nativePc")
         else -> throw GradleException("Host OS is not supported in Kotlin/Native.")
     }
 
-    
+
     sourceSets {
-        val commonMain by getting
-        val commonTest by getting {
+        // 共享代码
+        val commonMain by getting {
             dependencies {
-                implementation(kotlin("test"))
+                //put your multiplatform dependencies here
             }
         }
-        val jvmMain by getting
-        val jvmTest by getting
-        val jsMain by getting
-        val jsTest by getting
-        val nativeMain by getting
-        val nativeTest by getting
+        val commonTest by getting {
+            dependencies {
+                implementation(libs.kotlin.test)
+            }
+        }
+
+        // android代码
+        val androidMain by getting {
+            dependsOn(commonMain)
+        }
+
+        // ios代码
+        val iosMain by getting {
+            dependsOn(commonMain)
+        }
+
+        // native代码
+        val nativePcMain by getting {
+            dependsOn(commonMain)
+        }
     }
 }
+
+android {
+    namespace = "com.subscribe.kmpproject"
+    compileSdk = 34
+    defaultConfig {
+        minSdk = 21
+    }
+}
+
